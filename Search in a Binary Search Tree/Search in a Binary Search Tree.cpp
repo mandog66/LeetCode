106809f42f8b51642a8770c4c3d38a46c706f514
mandog66 <53986210+mandog66@users.noreply.github.com>
2024-06-14 19:14:51 +0800

    Search in a Binary Search Tree done

---
+++ Search in a Binary Search Tree/Search in a Binary Search Tree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *searchBST(TreeNode *root, int val)
+{
+    if (!root || root->val == val)
+        return root;
+
+    if (val > root->val)
+        return searchBST(root->right, val);
+    else
+        return searchBST(root->left, val);
+}
+
+int main(int argc, char *argv[])
+{
+    TreeNode *n1 = new TreeNode(4);
+    TreeNode *n2 = new TreeNode(2);
+    TreeNode *n3 = new TreeNode(7);
+    TreeNode *n4 = new TreeNode(1);
+    TreeNode *n5 = new TreeNode(3);
+
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+
+    std::cout << searchBST(n1, 2)->val << std::endl;
+}
